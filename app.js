dbaee368ff29282b8c78d2659e974b5af1490595
BugBuster <devrath.rana@bold.com>
2025-06-23 21:01:16 +0530

    dynamic import

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const util = require("./utils");
 
-console.log(util);
+(async () => {
+  const { square } = await import("./module.mjs"); // Importing a named export from a module which exports in es6 as require is not supported for such exported files so this is a work around
+  // const { square } = require("./module.mjs"); // This will not work as require does not support ES6 module syntax
+  // const m2 = require("./module2.mjs"); // This will not work as require does not support ES6 module syntax
+  // const { cube, pow4 } = m2; // This will not work as    require does not support ES6 module syntax  so this is a work around using dynamic imports
+  
+  console.log(util);
+  console.log(square(2)); // 4
+})();
