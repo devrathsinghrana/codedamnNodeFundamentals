b316d12c0d859cde4affd54a72034d7140e7dfed
BugBuster <devrath.rana@bold.com>
2025-06-23 20:56:08 +0530

    mjs extension essential for es6 to work

--- esimports.mjs
+++ esimports.mjs
@@ -1,6 +1,8 @@
 import * as m from "./module.mjs";
 import defaultValue from "./module.mjs";
 
+// we need .mjs extension for the import to work
+// or set "type": "module" in package.json
 
 console.log(m.square(2)); // 4
 console.log(m.m2.cube(3)); // 27
